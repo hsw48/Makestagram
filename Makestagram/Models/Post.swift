e7b560f8543ae3f730afc0bd9480962afb99ac01
Harrison Woodward <harrisonwoodward48@gmail.com>
2016-06-27 16:41:28 -0700

    Photo uploading to Parse enabled
    First commit

---
+++ Makestagram/Models/Post.swift
@@ -0,0 +1,63 @@
+//
+//  Post.swift
+//  Makestagram
+//
+//  Created by Harrison Woodward on 6/27/16.
+//  Copyright © 2016 Harrison Woodward. All rights reserved.
+//
+
+
+import Foundation
+import Parse
+
+// 1
+class Post : PFObject, PFSubclassing {
+    
+    // 2
+    @NSManaged var imageFile: PFFile?
+    @NSManaged var user: PFUser?
+    var image: UIImage?
+    
+    var photoUploadTask : UIBackgroundTaskIdentifier?
+    
+    func uploadPost() {
+        if let image = image {
+            guard let imageData = UIImageJPEGRepresentation(image, 0.8) else {return}
+            guard let imageFile = PFFile(name: "image.jpg",data: imageData) else {return}
+            
+            user = PFUser.currentUser()
+            self.imageFile = imageFile
+            
+            photoUploadTask = UIApplication.sharedApplication().beginBackgroundTaskWithExpirationHandler { () -> Void in
+                UIApplication.sharedApplication().endBackgroundTask(self.photoUploadTask!)
+            }
+            saveInBackgroundWithBlock() { (success: Bool, error: NSError?) in
+                UIApplication.sharedApplication().endBackgroundTask(self.photoUploadTask!)
+            }
+        }
+    }
+    
+    
+    
+    
+    //MARK: PFSubclassing Protocol
+    
+    // 3
+    static func parseClassName() -> String {
+        return "Post"
+    }
+    
+    // 4
+    override init () {
+        super.init()
+    }
+    
+    override class func initialize() {
+        var onceToken : dispatch_once_t = 0;
+        dispatch_once(&onceToken) {
+            // inform Parse about this subclass
+            self.registerSubclass()
+        }
+    }
+    
+}
